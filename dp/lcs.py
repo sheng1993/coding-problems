d44925d330e348226fb599d1e7ed35516e71fd4c
slin <slin@mobilize.net>
2019-08-20 06:25:57 -0600

    changes

--- dp/lcs.py
+++ dp/lcs.py
@@ -1,12 +1,14 @@
 # Longest Common Subsequence
 
+
 def lcs_naive(str1, str2, m, n):
     if m == 0 or n == 0:
         return 0
     elif str1[m-1] == str2[n-1]:
         return 1 + lcs_naive(str1, str2, m-1, n-1)
     else:
-        return max(lcs_naive(str1, str2, m, n-1), lcs_naive(str1, str2, m-1, n))
+        return max(lcs_naive(str1, str2, m, n-1),
+                   lcs_naive(str1, str2, m-1, n))
 
 
 def lcs_tabulation(str1, str2):
@@ -23,5 +25,5 @@ def lcs_tabulation(str1, str2):
                 L[i][j] = 1 + L[i-1][j-1]
             else:
                 L[i][j] = max(L[i][j-1], L[i-1][j])
-    
+
     return L[m][n]

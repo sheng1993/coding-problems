e1c1a8cffb5639a088543440823d1c71908d9e3a
slin <slin@mobilize.net>
2020-01-06 14:36:41 -0600

    info

---
+++ notebook_01.py
@@ -0,0 +1,18 @@
+# Microservices
+# A properly implemented microservices-based application can achieve the following goals:
+
+# Define strong contracts between the various microservices.
+# Allow for independent deployment cycles, including rollback.
+# Facilitate concurrent, A/B release testing on subsystems.
+# Minimize test automation and quality-assurance overhead.
+# Improve clarity of logging and monitoring.
+# Provide fine-grained cost accounting.
+# Increase overall application scalability and reliability.
+
+
+# HTTP vs HTTPS
+# HTTP lacks security mechanism to encrypt the data whereas HTTPS provides SSL or TLS Digital Certificate to secure the communication between server and client.
+# HTTP operates at Application Layer whereas HTTPS operates at Transport Layer.
+# HTTP by default operates on port 80 whereas HTTPS by default operates on port 443.
+# HTTP transfers data in plain text while HTTPS transfers data in cipher text (encrypt text).
+# HTTP is fast as compared to HTTPS because HTTPS consumes computation power to encrypt the communication channel.
